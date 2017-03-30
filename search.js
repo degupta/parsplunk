f646e81e0a7c8e7ec150c4326191de80c4cedecd
Devansh Gupta <devansh@wearableintelligence.com>
2017-03-30 16:30:51 -0700

    Display jobs

--- search.js
+++ search.js
@@ -9,9 +9,35 @@ function doSearch(qry) {
 			}
 		},
 		success: function(data) {
-			console.log(data);
+			updateUI(data);
 		},
 		dataType: 'json',
 		crossDomain: true,
 	})
-} 
+}
+
+function updateUI(data) {
+	var mainDiv = $("#rightTab")[0];
+	$(mainDiv).empty();
+	data.hits.hits.forEach(function(hit) {
+		var code = document.createElement("code");
+		code.className = 'prettyprint';
+
+		var mapped = hit._source;
+		delete mapped['creatorId']
+		delete mapped['teamId']
+
+		mapped["roles"] = mapped["roles"].map(function(role) {
+			return role.name;
+		})
+		mapped["users"] = mapped["users"].map(function(user) {
+			return user.email;
+		})
+
+		code.innerHTML = JSON.stringify(mapped);
+		newDiv = document.createElement("div");
+		newDiv.style = 'margin:20px';
+		newDiv.append(code);
+		mainDiv.append(newDiv);
+	});
+}
