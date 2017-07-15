69c71d44ecba9efdd9cd34ad94783a42cce14584
Devansh Gupta <devansh.gupta@swiggy.in>
2017-07-16 01:24:28 +0530

    Fix query

--- hack/injest.rb
+++ hack/injest.rb
@@ -13,27 +13,21 @@ i = 0
 gh = {}
 max = 0
 
-bad_hash = Set.new([
-'tdr1w99',
-'tdr1wd0',
-'tdr1w5z',
-'tdr1wdf',
-'tdr1we8',
-'tdr1w3q',
-'tdr1w7z',
-'tdr1w7x',
-'tepepxn',
-'tdr1w7b',
-'tdr1w44',
-'tdr1w4c',
-'tdr1wd8'
-])
+bad_hash = Set.new(['tdr1we0', 'tdr1wds', 'tdr1wdm', 'tdr1w99', 'tdr1w7v', 'tdr1w7q', 'tdr1w09', 'tdr1w77', 'tdr1we8', 'tdr1w98', 'tdr1tfp', 'tdr1w42', 'tdr1we9', 'tdr1w0c', 'tdr1w7h', 'tdr1we3', 'tdr1w6c', 'tdr1w6b', 'tdr1w3q', 'tdr1we2', 'tdr1wd0', 'tdr1w4b', 'tdr1wkn', 'tdr1w4c', 'tdr1w10', 'tdr1w40', 'tdr1wdf', 'tdr1wd1', 'tdr1w6g', 'tdr1w7t', 'tdr1w7x', 'tdr1tcz', 'tdr1w44', 'tdr1w7r', 'tdr1w5z', 'tdr1w13', 'tdr1w7y', 'tdr1w7b', 'tdr1w43', 'tdr1wde', 'tdr1wd2', 'tdr1w6z', 'tdr1w9g', 'tdr1wk1', 'tdr1w5b', 'tdr1w53', 'tdr1wkg', 'tdr1w50', 'tdr1tfm', 'tdr1w5h', 'tdr1wkp', 'tdr1w7z', 'tdr1w78', 'tdr1wdj', 'tdr1w1f', 'tdr1tfr', 'tdr1tcv', 'tdr1wdc', 'tdr1w51', 'tdr1w55', 'tdr1wd3', 'tdr1w49', 'tdr1wdb', 'tdr1w7w', 'teperb6', 'tepepy9', 'teper85', 'tepepwt', 'tepepx5', 'tepepxg', 'tepepxr', 'tepepxq', 'teperb0', 'teper6j', 'tepepwv', 'tepepxn', 'tepepwu', 'tepepwj', 'tepepz6', 'tepepzb', 'tepepyg', 'teper2p', 'teper8k', 'tdr1we0', 'tdr1wds', 'tdr1wdm', 'tdr1w99', 'tdr1w7v', 'tdr1w7q', 'tdr1w09', 'tdr1w77', 'tdr1we8', 'tdr1w98', 'tdr1tfp', 'tdr1w42', 'tdr1we9', 'tdr1w0c', 'tdr1w7h', 'tdr1we3', 'tdr1w6c', 'tdr1w6b', 'tdr1w3q', 'tdr1we2', 'tdr1wd0', 'tdr1w4b', 'tdr1wkn', 'tdr1w4c', 'tdr1w10', 'tdr1w40', 'tdr1wdf', 'tdr1wd1', 'tdr1w6g', 'tdr1w7t', 'tdr1w7x', 'tdr1tcz', 'tdr1w44', 'tdr1w7r', 'tdr1w5z', 'tdr1w13', 'tdr1w7y', 'tdr1w7b', 'tdr1w43', 'tdr1wde', 'tdr1wd2', 'tdr1w6z', 'tdr1w9g', 'tdr1wk1', 'tdr1w5b', 'tdr1w53', 'tdr1wkg', 'tdr1w50', 'tdr1tfm', 'tdr1w5h', 'tdr1wkp', 'tdr1w7z', 'tdr1w78', 'tdr1wdj', 'tdr1w1f', 'tdr1tfr', 'tdr1tcv', 'tdr1wdc', 'tdr1w51', 'tdr1w55', 'tdr1wd3', 'tdr1w49', 'tdr1wdb', 'tdr1w7w', 'teperb6', 'tepepy9', 'teper85', 'tepepwt', 'tepepx5', 'tepepxg', 'tepepxr', 'tepepxq', 'teperb0', 'teper6j', 'tepepwv', 'tepepxn', 'tepepwu', 'tepepwj', 'tepepz6', 'tepepzb', 'tepepyg', 'teper2p', 'teper8k'])
+
+ghc = {}
+ghr = {}
+order_count = 0
+avg_sla = 0
+item_count = 0
 
 File.open('orders.csv', 'r').read.each_line do |line|
   parts = line.split("@@")
 
   if doc && parts[0] != doc[:id]
-    doc[:body][:sla_bad] += 1 if doc[:body][:items].size > 4
+    item_count += doc[:body][:items].size
+    doc[:body][:sla_bad] += 1 if doc[:body][:items].size > 3
+    avg_sla += doc[:body][:sla_bad]
     client.create(doc)
     doc = nil
   end
@@ -47,6 +41,7 @@ File.open('orders.csv', 'r').read.each_line do |line|
   }
 
   if doc == nil
+    order_count += 1
     ordered_time = parts[1].to_i * 1000
     doc = {
       index: 'orders',
@@ -79,12 +74,14 @@ File.open('orders.csv', 'r').read.each_line do |line|
     }
     geo_cust = GeoHash.encode(doc[:body][:customer_location][:lat], doc[:body][:customer_location][:lon], 7)
     geo_rest = GeoHash.encode(doc[:body][:customer_location][:lat], doc[:body][:customer_location][:lon], 7)
+    ghc[geo_cust] = (ghc[geo_cust] || 0) + 1
+    ghr[geo_rest] = (ghr[geo_rest] || 0) + 1
 
     sla_bad = 0
     sla_bad += 1 if bad_hash.include?(geo_cust)
     sla_bad += 1 if bad_hash.include?(geo_rest)
-    sla_bad += 1 if ordered_time >= 1491062738 && ordered_time <= 1491062738 + 1500
-    doc[:body][:sla_bad] = parts[1].to_i
+    sla_bad += 1 if Time.at(ordered_time).hour >= 20 && Time.at(ordered_time).hour <= 23
+    doc[:body][:sla_bad] = sla_bad
   else
     doc[:body][:items] << item
   end
@@ -95,4 +92,11 @@ File.open('orders.csv', 'r').read.each_line do |line|
   end
 end
 
+ghc.each { |k,v| print "'#{k}', " if v >= 100}
+ghr.each { |k,v| print "'#{k}', " if v >= 100}
+
+puts
+puts
+
+puts "Order Count: #{order_count}, Avg Sla: #{avg_sla.to_f / order_count}, Avg. item Count:  #{item_count.to_f / order_count}"
 client.create(doc)
--- hack/query.sql
+++ hack/query.sql
@@ -1,10 +1,30 @@
 select od.order_id, UNIX_TIMESTAMP(ordered_time), payment_status, customer_id, customer_lat, customer_lng,
        customer_user_agent, payment_method, od.restaurant_id, item_id, quantity, r.name,
        cuisine, area_id, r.city_id, lat, lng, avg_rating, item_name, is_enabled, item_price, a.name
-  from order_details od
-  inner join order_items oi on od.order_id = od.order_id
-  inner join restaurants r  on r.id = od.restaurant_id
-  inner join restaurant_menu rm on rm.id = oi.item_id
-  inner join area a on a.id = r.area_id
-  limit 100000
+from order_details od
+inner join order_items oi on oi.order_id = od.order_id
+inner join restaurants r  on r.id = od.restaurant_id
+inner join restaurant_menu rm on rm.id = oi.item_id
+inner join area a on a.id = r.area_id
+where ordered_time <= '2017-04-10 00:00:00'
   INTO OUTFILE '~/Downloads/parsplunk/hack/orders.csv' FIELDS TERMINATED BY '@@' LINES TERMINATED BY '\n';
+
+
+ALTER TABLE order_details
+   ADD CONSTRAINT `order_details_r_fk`
+   FOREIGN KEY (`restaurant_id` )
+   REFERENCES `restaurants` (`id` )
+   ON DELETE CASCADE;
+
+
+ALTER TABLE order_items
+   ADD CONSTRAINT `order_items_fk_1`
+   FOREIGN KEY (`order_id` )
+   REFERENCES `order_details` (`order_id` )
+   ON DELETE CASCADE;
+
+ALTER TABLE order_items
+   ADD CONSTRAINT `order_items_fk_2`
+   FOREIGN KEY (`item_id` )
+   REFERENCES `restaurant_menu` (`id` )
+   ON DELETE CASCADE;
